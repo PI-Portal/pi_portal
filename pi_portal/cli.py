f83461d29feaa83786a8580f5144337f1b1cab25
Niall Byrne <niall@niallbyrne.ca>
2022-02-03 19:59:33 +0000

    refactor(LOGGER): extract class/move uuid to state

--- pi_portal/cli.py
+++ pi_portal/cli.py
@@ -1,4 +1,4 @@
-"""The Pi Portal Door DoorMonitor Application."""
+"""The Pi Portal CLI."""
 
 import click
 from . import config
@@ -6,58 +6,72 @@ from .modules import configuration, general, integrations, system
 
 
 @click.group()
-def cli():
-  """Door Monitor CLI."""
+@click.pass_context
+def cli(ctx: click.Context) -> None:
+  """Door Monitor CLI.
+
+  :param ctx: The passed click context object.
+  """
+
+  running_state = configuration.state.State()
+  running_state.load()
+  ctx.obj = {
+      'running_state': running_state,
+      'logging_config': general.LoggingConfiguration()
+  }
 
 
 @cli.command("monitor")
-def monitor():
-  """Begin monitoring the door."""
-  configuration.state.State().load()
+@click.pass_context
+def monitor(ctx: click.Context) -> None:
+  """Begin monitoring the door.
+
+  :param ctx: The passed click context object.
+  """
+
   door_monitor = integrations.door_monitor.DoorMonitor()
-  door_monitor.log = general.logger.setup_logger(
-      door_monitor.log, config.LOGFILE_PATH
+  door_monitor.log = ctx.obj['logging_config'].configure(
+      door_monitor.log,
+      config.LOGFILE_PATH,
   )
   door_monitor.start()
 
 
 @cli.command("slack_bot")
-def slack_bot():
+def slack_bot() -> None:
   """Connect the interactive Slack bot."""
-  configuration.state.State().load()
+
   slack_client = integrations.slack.Client()
   slack_client.subscribe()
 
 
 @cli.command("upload_snapshot")
 @click.argument('filename', type=click.Path(exists=True))
-def upload_snapshot(filename: str):
+def upload_snapshot(filename: str) -> None:
   """Upload a snapshot image to Slack.
 
   :param filename: The path to the file to upload.
   """
 
-  configuration.state.State().load()
   slack_client = integrations.slack.Client()
   slack_client.send_snapshot(filename)
 
 
 @cli.command("upload_video")
 @click.argument('filename', type=click.Path(exists=True))
-def upload_video(filename: str):
+def upload_video(filename: str) -> None:
   """Upload a video to Slack and S3.
 
   :param filename: The path to the file to upload.
   """
 
-  configuration.state.State().load()
   slack_client = integrations.slack.Client()
   slack_client.send_video(filename)
 
 
 @cli.command("installer")
 @click.argument('config_file', type=click.Path(exists=True))
-def installer(config_file: str):
+def installer(config_file: str) -> None:
   """Run the installation script, (requires root).
 
   :param config_file: The path to the config file to use.
