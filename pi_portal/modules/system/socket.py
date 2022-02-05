c30073d935029ec4de10cbc08551087fcb62ab6d
Niall Byrne <niall@niallbyrne.ca>
2022-02-05 16:16:07 +0000

    refactor(SOCKET): types, docstrings

--- pi_portal/modules/system/socket.py
+++ pi_portal/modules/system/socket.py
@@ -1,4 +1,4 @@
-"""Provide a unix stream transport for xmlrpc."""
+"""Unix stream transport for XMLRPC."""
 
 import http.client
 import socket
@@ -9,24 +9,30 @@ from typing import Dict, Tuple, Union
 class UnixStreamHTTPConnection(http.client.HTTPConnection):
   """An http connection over a unix socket."""
 
-  def connect(self):
-    """Open a unix socket to the specified host."""
+  def connect(self) -> None:
+    """Open a unix socket on the specified host."""
 
     self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     self.sock.connect(self.host)
 
 
 class UnixStreamTransport(xmlrpc.client.Transport):
-  """An xmlrpc client Transport using a unix socket."""
+  """An XMLRPC client transport using a unix socket.
 
-  def __init__(self, socket_path: str):
+  :param socket_path: Path to the unix socket to open.
+  """
+
+  def __init__(self, socket_path: str) -> None:
     self.socket_path = socket_path
     super(UnixStreamTransport, self).__init__()  # pylint: disable=super-with-arguments
 
-  def make_connection(self, host: Union[Tuple[str, Dict[str, str]], str]):
+  def make_connection(
+      self, host: Union[Tuple[str, Dict[str, str]], str]
+  ) -> UnixStreamHTTPConnection:
     """Connect to the specified unix socket, ignoring the host parameter.
 
     :param host: Unused parameter in this context.
+    :returns: A http connection over a unix socket.
     """
 
     return UnixStreamHTTPConnection(self.socket_path)
