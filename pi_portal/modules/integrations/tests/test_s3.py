75b804e40670f485818e07ed84689642c478297c
Niall Byrne <niall@niallbyrne.ca>
2022-02-05 15:58:57 +0000

    refactor(S3): types, docstrings

--- pi_portal/modules/integrations/tests/test_s3.py
+++ pi_portal/modules/integrations/tests/test_s3.py
@@ -13,20 +13,20 @@ class TestS3Bucket(TestCase):
   """Test the S3Bucket class."""
 
   @mock_state.patch
-  @mock.patch(s3.__name__ + ".boto3")
-  def setUp(self, m_boto):  # pylint: disable=arguments-differ
-    self.s3_client = s3.S3Bucket()
-    self.s3_client.boto_client = m_boto.return_value
+  def setUp(self) -> None:
+    with mock.patch(s3.__name__ + ".boto3") as m_boto:
+      self.s3_client = s3.S3Bucket()
+      self.s3_client.boto_client = m_boto.return_value
 
   @mock_state.patch
-  def test_initialization(self):
+  def test_initialization(self) -> None:
     s3_client = s3.S3Bucket()
     self.assertEqual(s3_client.bucket_name, mock_state.MOCK_S3_BUCKET_NAME)
     self.assertIsInstance(s3_client.boto_client, BaseClient)
 
   @mock_state.patch
   @mock.patch(s3.__name__ + ".boto3")
-  def test_boto_client_initialization(self, m_boto):
+  def test_boto_client_initialization(self, m_boto: mock.Mock) -> None:
     s3.S3Bucket()
     m_boto.client.assert_called_once_with(
         's3',
@@ -34,7 +34,7 @@ class TestS3Bucket(TestCase):
         aws_secret_access_key=mock_state.MOCK_AWS_SECRET_ACCESS_KEY,
     )
 
-  def test_upload_file(self):
+  def test_upload_file(self) -> None:
     mock_file_name = "/dir/mock_file_name.mp4"
     self.s3_client.upload(mock_file_name)
     self.s3_client.boto_client.upload_file.assert_called_once_with(
@@ -43,7 +43,7 @@ class TestS3Bucket(TestCase):
         os.path.basename(mock_file_name),
     )
 
-  def test_upload_file_exception(self):
+  def test_upload_file_exception(self) -> None:
     self.s3_client.boto_client.upload_file.side_effect = ClientError(
         error_response={}, operation_name="BOOM!"
     )
