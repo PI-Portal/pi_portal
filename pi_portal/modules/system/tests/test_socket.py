6b39b2fef0fc5ae804bd10c022aed1a0c179d942
Niall Byrne <niall@niallbyrne.ca>
2022-02-05 16:16:07 +0000

    refactor(SOCKET): types, docstrings

--- pi_portal/modules/system/tests/test_socket.py
+++ pi_portal/modules/system/tests/test_socket.py
@@ -8,15 +8,15 @@ from pi_portal.modules.system import socket
 class TestUnixStreamTransport(TestCase):
   """Test the UnixStreamTransport class."""
 
-  def setUp(self):
+  def setUp(self) -> None:
     self.mock_path = '/var/run/mock.socket'
     self.transport = socket.UnixStreamTransport(self.mock_path)
 
-  def test_initialize(self):
+  def test_initialize(self) -> None:
     self.assertEqual(self.transport.socket_path, self.mock_path)
 
   @mock.patch(socket.__name__ + ".UnixStreamHTTPConnection")
-  def test_make_connection(self, m_http_stream):
+  def test_make_connection(self, m_http_stream: mock.Mock) -> None:
     connection = self.transport.make_connection("https://unused/argument")
     self.assertEqual(connection, m_http_stream.return_value)
     m_http_stream.assert_called_once_with(self.mock_path)
@@ -25,12 +25,12 @@ class TestUnixStreamTransport(TestCase):
 class TestUnixStreamHTTPConnection(TestCase):
   """Test the UnixStreamHTTPConnection class."""
 
-  def setUp(self):
+  def setUp(self) -> None:
     self.mock_path = '/var/run/mock.socket'
     self.connection = socket.UnixStreamHTTPConnection(self.mock_path)
 
   @mock.patch(socket.__name__ + ".socket.socket")
-  def test_connect(self, m_socket):
+  def test_connect(self, m_socket: mock.Mock) -> None:
     self.connection.connect()
     m_socket.assert_called_once_with(
         socket.socket.AF_UNIX, socket.socket.SOCK_STREAM
