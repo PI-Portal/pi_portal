796b09dcde394d06e68974f6992b194199e659e0
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-12-14 15:38:11 +0000

    feat(CICD-TOOLS): add pre-commit/move dependencies

--- pi_portal/stubs/pythonjsonlogger/jsonlogger.pyi
+++ pi_portal/stubs/pythonjsonlogger/jsonlogger.pyi
@@ -3,6 +3,13 @@
 import logging
 from typing import Any, Dict, Optional
 
+# isort: off
+
+
 class JsonFormatter(logging.Formatter):
 
-  def add_fields(self, log_record: Dict[str, Any], record: logging.LogRecord, message_dict: Dict[str, Optional[str]]) -> None: ...
+  def add_fields(
+      self, log_record: Dict[str, Any], record: logging.LogRecord,
+      message_dict: Dict[str, Optional[str]]
+  ) -> None:
+    ...
