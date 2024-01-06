767d8f56c132e9a598b1f0b789da92a77cd9c54a
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-01-06 21:10:00 +0000

    test(SUPERVISOR): refactor tests to use pytest fixtures

--- pi_portal/modules/system/tests/test_supervisor.py
+++ pi_portal/modules/system/tests/test_supervisor.py
@@ -1,97 +1,139 @@
-"""Test Supervisor SlackClient Class."""
+"""Test the SupervisorClient Class."""
 
 from typing import cast
-from unittest import TestCase, mock
+from unittest import mock
 
+import pytest
 from pi_portal.modules.python.xmlrpc import patched_client
 from pi_portal.modules.system import supervisor, supervisor_config
 
 
-class TestSupervisorClient(TestCase):
+class TestSupervisorClient:
   """Test the SupervisorClient class."""
 
-  mock_state = supervisor_config.ProcessStatus("RUNNING")
+  mock_status = supervisor_config.ProcessStatus("RUNNING")
   mock_start = "00:00:00"
+  mock_process_info = supervisor.TypeSupervisorProcessInfo(
+      statename=mock_status,
+      start=mock_start,
+  )
 
-  def setUp(self) -> None:
-    self.supervisor_client = supervisor.SupervisorClient()
-    self.supervisor_client.server = mock.MagicMock()
-
-  def _mock_server(self) -> mock.Mock:
-    return cast(mock.Mock, self.supervisor_client.server)
-
-  def create_mock_process_info(self) -> supervisor.TypeSupervisorProcessInfo:
-    return supervisor.TypeSupervisorProcessInfo(
-        statename=self.mock_state,
-        start=self.mock_start,
+  def test_initialize__attributes(
+      self,
+      mocked_supervisor_server: mock.Mock,
+      supervisor_instance: supervisor.SupervisorClient,
+  ) -> None:
+    assert supervisor_instance.server == cast(
+        patched_client.Server,
+        mocked_supervisor_server.return_value,
     )
 
-  def test_initialize(self) -> None:
-    client = supervisor.SupervisorClient()
-    self.assertIsInstance(client.server, patched_client.Server)
+  def test_start__success__calls_server_method(
+      self,
+      mocked_supervisor_server: mock.Mock,
+      supervisor_instance: supervisor.SupervisorClient,
+  ) -> None:
+    supervisor_instance.start(supervisor_config.ProcessList.CAMERA)
 
-  def test_start(self) -> None:
-    self.supervisor_client.start(supervisor_config.ProcessList.CAMERA)
-    self._mock_server().supervisor.startProcess.\
-      assert_called_once_with(
-        supervisor_config.ProcessList.CAMERA.value
-      )
+    mocked_supervisor_server.return_value.supervisor.startProcess.\
+        assert_called_once_with(
+          supervisor_config.ProcessList.CAMERA.value
+        )
 
-  def test_start_error(self) -> None:
-    self._mock_server().supervisor.startProcess.side_effect = (
-        patched_client.Fault(1, "MockFaultError")
-    )
-    with self.assertRaises(supervisor.SupervisorException):
-      self.supervisor_client.start(supervisor_config.ProcessList.CAMERA)
+  def test_start__error__throws_exception(
+      self,
+      mocked_supervisor_server: mock.Mock,
+      supervisor_instance: supervisor.SupervisorClient,
+  ) -> None:
+    mocked_supervisor_server.return_value.supervisor.startProcess.\
+        side_effect = (
+          patched_client.Fault(1, "MockFaultError")
+        )
+    with pytest.raises(supervisor.SupervisorException):
 
-  def test_stop(self) -> None:
-    self.supervisor_client.stop(supervisor_config.ProcessList.CAMERA)
-    self._mock_server().supervisor.stopProcess.\
-      assert_called_once_with(
-        supervisor_config.ProcessList.CAMERA.value
-      )
+      supervisor_instance.start(supervisor_config.ProcessList.CAMERA)
 
-  def test_stop_error(self) -> None:
-    self._mock_server().supervisor.stopProcess.side_effect = (
-        patched_client.Fault(1, "MockFaultError")
-    )
-    with self.assertRaises(supervisor.SupervisorException):
-      self.supervisor_client.stop(supervisor_config.ProcessList.CAMERA)
+  def test_stop__success__calls_server_method(
+      self,
+      mocked_supervisor_server: mock.Mock,
+      supervisor_instance: supervisor.SupervisorClient,
+  ) -> None:
+    supervisor_instance.stop(supervisor_config.ProcessList.CAMERA)
 
-  def test_status(self) -> None:
-    self._mock_server().supervisor.getProcessInfo.return_value = \
-      self.create_mock_process_info()
-    result = self.supervisor_client.status(supervisor_config.ProcessList.CAMERA)
-    self._mock_server().supervisor.getProcessInfo.\
-      assert_called_once_with(
-        supervisor_config.ProcessList.CAMERA.value
-      )
-    self.assertEqual(result, self.mock_state)
+    mocked_supervisor_server.return_value.supervisor.stopProcess.\
+        assert_called_once_with(
+          supervisor_config.ProcessList.CAMERA.value
+        )
 
-  def test_status_error(self) -> None:
-    self._mock_server().supervisor.getProcessInfo.side_effect = (
-        patched_client.Fault(1, "MockFaultError")
-    )
-    with self.assertRaises(supervisor.SupervisorException):
-      self.supervisor_client.status(supervisor_config.ProcessList.CAMERA)
+  def test_stop__error__throws_exception(
+      self,
+      mocked_supervisor_server: mock.Mock,
+      supervisor_instance: supervisor.SupervisorClient,
+  ) -> None:
+    mocked_supervisor_server.return_value.supervisor.stopProcess.\
+        side_effect = (
+            patched_client.Fault(1, "MockFaultError")
+        )
+    with pytest.raises(supervisor.SupervisorException):
 
-  def test_start_time(self) -> None:
-    self._mock_server().supervisor.getProcessInfo.return_value = \
-      self.create_mock_process_info()
-    result = self.supervisor_client.start_time(
+      supervisor_instance.stop(supervisor_config.ProcessList.CAMERA)
+
+  def test_status__success__returns_correct_status(
+      self,
+      mocked_supervisor_server: mock.Mock,
+      supervisor_instance: supervisor.SupervisorClient,
+  ) -> None:
+    mocked_supervisor_server.return_value.supervisor.getProcessInfo.\
+        return_value = self.mock_process_info
+
+    result = supervisor_instance.status(supervisor_config.ProcessList.CAMERA)
+
+    mocked_supervisor_server.return_value.supervisor.getProcessInfo.\
+        assert_called_once_with(
+          supervisor_config.ProcessList.CAMERA.value
+        )
+    assert result == self.mock_status
+
+  def test_status__error__throws_exception(
+      self,
+      mocked_supervisor_server: mock.Mock,
+      supervisor_instance: supervisor.SupervisorClient,
+  ) -> None:
+    mocked_supervisor_server.return_value.supervisor.getProcessInfo.\
+        side_effect = (
+          patched_client.Fault(1, "MockFaultError")
+        )
+    with pytest.raises(supervisor.SupervisorException):
+
+      supervisor_instance.status(supervisor_config.ProcessList.CAMERA)
+
+  def test_start_time__success__returns_correct_time(
+      self,
+      mocked_supervisor_server: mock.Mock,
+      supervisor_instance: supervisor.SupervisorClient,
+  ) -> None:
+    mocked_supervisor_server.return_value.supervisor.getProcessInfo.\
+        return_value = self.mock_process_info
+
+    result = supervisor_instance.start_time(
         supervisor_config.ProcessList.DOOR_MONITOR
     )
-    self._mock_server().supervisor.getProcessInfo.\
-      assert_called_once_with(
-        supervisor_config.ProcessList.DOOR_MONITOR.value
-      )
-    self.assertEqual(result, self.mock_start)
 
-  def test_start_time_error(self) -> None:
-    self._mock_server().supervisor.getProcessInfo.side_effect = (
-        patched_client.Fault(1, "MockFaultError")
-    )
-    with self.assertRaises(supervisor.SupervisorException):
-      self.supervisor_client.start_time(
-          supervisor_config.ProcessList.DOOR_MONITOR
-      )
+    mocked_supervisor_server.return_value.supervisor.getProcessInfo.\
+        assert_called_once_with(
+          supervisor_config.ProcessList.DOOR_MONITOR.value
+        )
+    assert result == self.mock_start
+
+  def test_start_time__error__throws_exception(
+      self,
+      mocked_supervisor_server: mock.Mock,
+      supervisor_instance: supervisor.SupervisorClient,
+  ) -> None:
+    mocked_supervisor_server.return_value.supervisor.getProcessInfo.\
+        side_effect = (
+            patched_client.Fault(1, "MockFaultError")
+        )
+    with pytest.raises(supervisor.SupervisorException):
+
+      supervisor_instance.start_time(supervisor_config.ProcessList.DOOR_MONITOR)
