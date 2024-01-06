0cc7d5f0a1c7629985fe7b53bcaac6abd27ba828
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-01-06 20:25:46 +0000

    test(LINUX): refactor for readability

--- pi_portal/modules/system/tests/test_linux.py
+++ pi_portal/modules/system/tests/test_linux.py
@@ -1,19 +1,24 @@
 """Test Linux Utilities."""
 
-from unittest import TestCase, mock
+from unittest import mock
 
 from pi_portal.modules.system import linux
 
 
-class TestLinux(TestCase):
+class TestLinux:
   """Test the Linux utilities module."""
 
   mock_uptime = 52675.05
   mock_uptime_as_string = "14 hours"
 
   @mock.patch(linux.__name__ + ".time.monotonic")
-  def test_uptime(self, m_mono: mock.Mock) -> None:
-    m_mono.return_value = self.mock_uptime
+  def test_uptime__correct_value(
+      self,
+      m_monotonic: mock.Mock,
+  ) -> None:
+    m_monotonic.return_value = self.mock_uptime
+
     result = linux.uptime()
-    self.assertEqual(result, self.mock_uptime_as_string)
-    m_mono.assert_called_once_with()
+
+    assert result == self.mock_uptime_as_string
+    m_monotonic.assert_called_once_with()
