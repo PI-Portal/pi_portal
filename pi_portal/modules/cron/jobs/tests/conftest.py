264355b8b2ff169521fb09eb987b1e4a1b2c8545
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-01-17 13:55:02 +0000

    feat(CRON): log file archival job

--- pi_portal/modules/cron/jobs/tests/conftest.py
+++ pi_portal/modules/cron/jobs/tests/conftest.py
@@ -4,7 +4,7 @@ import logging
 
 import pytest
 from pi_portal.modules.configuration.tests.fixtures import mock_state
-from .. import dead_man_switch, video_upload
+from .. import dead_man_switch, log_upload, video_upload
 
 
 @pytest.fixture
@@ -12,6 +12,15 @@ def dead_man_switch_cron_job_instance(
     mocked_cron_logger: logging.Logger
 ) -> dead_man_switch.DeadManSwitchCronJob:
   return dead_man_switch.DeadManSwitchCronJob(mocked_cron_logger)
+
+
+@pytest.fixture
+def log_upload_cron_job_instance(
+    mocked_cron_logger: logging.Logger
+) -> log_upload.LogFileUploadCronJob:
+  with mock_state.mock_state_creator():
+    instance = log_upload.LogFileUploadCronJob(mocked_cron_logger)
+  return instance
 
 
 @pytest.fixture
