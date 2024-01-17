d4ec53edf99e4ed7dec900efe24cbea45f0d0120
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-01-17 09:13:44 +0000

    feat(CONFIG): manage multiple s3 buckets

--- pi_portal/modules/integrations/s3/client.py
+++ pi_portal/modules/integrations/s3/client.py
@@ -1,7 +1,5 @@
 """S3BucketClient class."""
 
-import os
-
 import boto3
 from botocore.exceptions import ClientError
 from pi_portal.modules.configuration import state
@@ -14,9 +12,9 @@ class S3BucketException(Exception):
 class S3BucketClient:
   """S3BucketClient class."""
 
-  def __init__(self) -> None:
+  def __init__(self, bucket_name: str) -> None:
     current_state = state.State()
-    self.bucket_name = current_state.user_config['S3_BUCKET_NAME']
+    self.bucket_name = bucket_name
     self.boto_client = boto3.client(
         's3',
         aws_access_key_id=current_state.user_config['AWS_ACCESS_KEY_ID'],
@@ -24,16 +22,23 @@ class S3BucketClient:
         user_config['AWS_SECRET_ACCESS_KEY'],
     )
 
-  def upload(self, file_name: str) -> None:
+  def upload(
+      self,
+      file_name: str,
+      object_name: str,
+  ) -> None:
     """Upload the specified file to the S3 bucket.
 
     :param file_name: The path of the file to upload.
+    :param object_name: The name of the S3 object that will be created.
     :raises: :class:`S3BucketException`
     """
 
     try:
       self.boto_client.upload_file(
-          file_name, self.bucket_name, os.path.basename(file_name)
+          file_name,
+          self.bucket_name,
+          object_name,
       )
     except ClientError as exc:
       raise S3BucketException from exc
