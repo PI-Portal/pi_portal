be6079ef4e7e666957edf294028e8a78fc1d4cd8
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-02-01 18:58:17 +0000

    feat(TASKS): add task to send chat message

---
+++ pi_portal/modules/tasks/task/chat_send_message.py
@@ -0,0 +1,29 @@
+"""A task to send a message with the chat client."""
+
+from dataclasses import dataclass
+
+from pi_portal.modules.tasks import enums
+from pi_portal.modules.tasks.task.bases import task_args_base, task_base
+from pi_portal.modules.tasks.task.metaclasses.meta_task import MetaTask
+from typing_extensions import TypeAlias
+
+ApiEnabled = True
+
+
+@dataclass
+class Args(task_args_base.TaskArgsBase):
+  """Arguments for chat send message tasks."""
+
+  message: str
+
+
+ReturnType: TypeAlias = None
+TaskType = enums.TaskType.CHAT_SEND_MESSAGE
+
+
+class Task(
+    task_base.TaskBase[Args, ReturnType],
+    metaclass=MetaTask,
+    task_type=TaskType,
+):
+  """A task to send a message with the chat client."""
