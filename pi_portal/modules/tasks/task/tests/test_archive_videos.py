c506499d9117d8dc7d6ef929603e6ede63fcbf14
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-02-02 03:13:49 +0000

    feat(TASKS): add validation to file system args

--- pi_portal/modules/tasks/task/tests/test_archive_videos.py
+++ pi_portal/modules/tasks/task/tests/test_archive_videos.py
@@ -1,7 +1,9 @@
 """Test the archive_videos module."""
 
+from pi_portal import config
 from pi_portal.modules.tasks import enums
 from pi_portal.modules.tasks.task import archive_videos
+from pi_portal.modules.tasks.task.shared import archive
 from pi_portal.modules.tasks.task.utility.generic_task_test import (
     GenericTaskModuleTest,
 )
@@ -14,8 +16,14 @@ class TestArchiveVideos(GenericTaskModuleTest):
   expected_arg_class = archive_videos.Args
   expected_return_type = None
   expected_type = enums.TaskType.ARCHIVE_VIDEOS
-  mock_args = archive_videos.Args(
-      archival_path="/var/lib/motion/mock.mp4",
-      partition_name="mock_partition",
-  )
+  mock_args = archive_videos.Args(partition_name="mock_partition")
   module = archive_videos
+
+  def test_import__args_class__attributes(self) -> None:
+    assert archive_videos.Args.archival_path == config.PATH_QUEUE_VIDEO_UPLOAD
+
+  def test_import__args_class__inheritance(self) -> None:
+    assert issubclass(
+        archive_videos.Args,
+        archive.ArchivalTaskArgs,
+    )
