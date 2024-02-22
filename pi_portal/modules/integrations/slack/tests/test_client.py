39ae2984660720ee7c3c7ad293fb74ae0b0816d9
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-02-22 19:53:35 +0000

    feat(MOTION): support multiple cameras

--- pi_portal/modules/integrations/slack/tests/test_client.py
+++ pi_portal/modules/integrations/slack/tests/test_client.py
@@ -5,7 +5,6 @@ from unittest import mock
 import pytest
 from pi_portal.modules.configuration import state
 from pi_portal.modules.configuration.tests.fixtures import mock_state
-from pi_portal.modules.integrations.motion import client as motion_client
 from pi_portal.modules.integrations.slack import client as slack_client
 from pi_portal.modules.integrations.slack import config as slack_config
 from slack_sdk.errors import SlackRequestError
@@ -132,58 +131,3 @@ class TestSlackClient:
         [
             mock.call().error("Failed to send file: '%s'", test_file),
         ] * client_instance.retries
-
-  def test_send_snapshot__success(
-      self,
-      client_instance: slack_client.SlackClient,
-      mocked_motion_client: mock.Mock,
-      mocked_slack_web_client: mock.Mock,
-  ) -> None:
-    test_snapshot = "/path/to/mock/snapshot.jpg"
-
-    client_instance.send_snapshot(test_snapshot)
-
-    mocked_slack_web_client.return_value.files_upload_v2.\
-        assert_called_once_with(
-          channel=client_instance.channel_id,
-          file=test_snapshot,
-          title=client_instance.config.upload_file_title
-        )
-    mocked_motion_client.return_value.cleanup_snapshot.\
-        assert_called_once_with(
-          test_snapshot
-        )
-
-  def test_send_snapshot__exception(
-      self,
-      client_instance: slack_client.SlackClient,
-      mocked_logger: mock.Mock,
-      mocked_motion_client: mock.Mock,
-      mocked_slack_web_client: mock.Mock,
-  ) -> None:
-    test_snapshot = "/path/to/mock/snapshot.jpg"
-    mocked_motion_client.return_value.cleanup_snapshot.side_effect = \
-        motion_client.MotionException("Boom!")
-
-    client_instance.send_snapshot(test_snapshot)
-
-    mocked_slack_web_client.return_value.files_upload_v2.\
-        assert_called_once_with(
-          channel=client_instance.channel_id,
-          file=test_snapshot,
-          title=client_instance.config.upload_file_title
-        )
-    mocked_slack_web_client.return_value.chat_postMessage.\
-        assert_called_once_with(
-          channel=client_instance.channel,
-          text="An error occurred cleaning up this snapshot.",
-        )
-    mocked_motion_client.return_value.cleanup_snapshot.\
-        assert_called_once_with(
-          test_snapshot
-        )
-    assert mocked_logger.mock_calls == \
-           [mock.call()] + \
-           [
-             mock.call().error("Failed to remove old motion snapshot!"),
-           ]
