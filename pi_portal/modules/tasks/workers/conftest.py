db9cf2b03c38548e6e02e36e249f9015ad65a6d1
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-02-25 14:16:11 +0000

    refactor(TASKS): simplify worker constructors

--- pi_portal/modules/tasks/workers/conftest.py
+++ pi_portal/modules/tasks/workers/conftest.py
@@ -1,8 +1,33 @@
 """Shared test fixtures for the cron modules tests."""
+# pylint: disable=redefined-outer-name
 
 import logging
+from unittest import mock
 
 import pytest
+from pi_portal.modules.tasks.enums import TaskManifests
+
+
+@pytest.fixture
+def mocked_failed_task_manifest() -> mock.Mock:
+  instance = mock.Mock()
+  instance.contents = []
+  return instance
+
+
+@pytest.fixture
+def mocked_task_scheduler(
+    mocked_failed_task_manifest: mock.Mock,
+    mocked_task_registry: mock.Mock,
+    mocked_task_router: mock.Mock,
+    mocked_worker_logger: logging.Logger,
+) -> mock.Mock:
+  instance = mock.Mock()
+  instance.registry = mocked_task_registry
+  instance.router = mocked_task_router()
+  instance.log = mocked_worker_logger
+  instance.manifests = {TaskManifests.FAILED_TASKS: mocked_failed_task_manifest}
+  return instance
 
 
 @pytest.fixture
