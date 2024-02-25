eb1fbc68828e622ebb83fb21accff8615ac581e2
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-02-25 00:36:52 +0000

    refactor(ARCHIVAL): extract abc, s3 implementation

---
+++ pi_portal/modules/integrations/archival/__init__.py
@@ -0,0 +1,7 @@
+"""Archival integration classes."""
+
+from pi_portal.modules.integrations.archival.bases.client import \
+    ArchivalClientBase as TypeArchivalClient
+from pi_portal.modules.integrations.archival.bases.client import (
+    ArchivalException,
+)
