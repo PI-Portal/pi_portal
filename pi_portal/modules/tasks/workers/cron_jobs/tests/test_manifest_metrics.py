e84942159d86609588f2a11c7e49ea60fe6ee728
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-02-25 14:53:18 +0000

    feat(TASKS): cron job to report manifest metrics

---
+++ pi_portal/modules/tasks/workers/cron_jobs/tests/test_manifest_metrics.py
@@ -0,0 +1,72 @@
+"""Test the manifest_metrics module."""
+
+from io import StringIO
+from unittest import mock
+
+from pi_portal import config
+from pi_portal.modules.tasks import enums
+from pi_portal.modules.tasks.task import non_scheduled
+from .. import manifest_metrics
+from ..bases import cron_job_base
+
+
+class TestManifestMetricsCronJob:
+  """Test the manifest_metrics module."""
+
+  log_message = (
+      "INFO - None - Manifest Metrics - {metrics} - "
+      "Metrics for the '{manifest}' task manifest.\n"
+  )
+
+  def test_initialize__attributes(
+      self,
+      manifest_metrics_cron_job_instance: manifest_metrics.CronJob,
+  ) -> None:
+    assert manifest_metrics_cron_job_instance.interval == \
+        config.CRON_INTERVAL_MANIFEST_METRICS
+    assert manifest_metrics_cron_job_instance.name == "Manifest Metrics"
+    assert manifest_metrics_cron_job_instance.quiet is True
+    assert manifest_metrics_cron_job_instance.type == \
+        enums.TaskType.NON_SCHEDULED
+    assert manifest_metrics_cron_job_instance.priority == \
+        enums.TaskPriority.STANDARD
+
+  def test_initialize__inheritance(
+      self,
+      manifest_metrics_cron_job_instance: manifest_metrics.CronJob,
+  ) -> None:
+    assert isinstance(
+        manifest_metrics_cron_job_instance,
+        cron_job_base.CronJobBase,
+    )
+
+  def test_args__returns_correct_value(
+      self,
+      manifest_metrics_cron_job_instance: manifest_metrics.CronJob,
+  ) -> None:
+    expected_args = non_scheduled.Args()
+
+    # pylint: disable=protected-access
+    assert manifest_metrics_cron_job_instance._args() == expected_args
+
+  def test_process__logging(
+      self,
+      manifest_metrics_cron_job_instance: manifest_metrics.CronJob,
+      mocked_task_scheduler: mock.Mock,
+      mocked_stream: StringIO,
+  ) -> None:
+    mocked_task_scheduler.manifests = {
+        manifest: mock.Mock() for manifest in enums.TaskManifests
+    }
+
+    manifest_metrics_cron_job_instance.schedule(mocked_task_scheduler)
+
+    assert mocked_stream.getvalue() == "".join(
+        [
+            self.log_message.format(
+                manifest=manifest.value,
+                metrics=mocked_task_scheduler.manifests[manifest].metrics.
+                return_value,
+            ) for manifest in enums.TaskManifests
+        ]
+    )
