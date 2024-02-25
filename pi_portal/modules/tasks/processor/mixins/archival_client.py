4843b7c1fcf3625e102d1582407e3216ec9253a6
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-02-25 00:36:52 +0000

    refactor(ARCHIVAL): extract abc, s3 implementation

--- pi_portal/modules/tasks/processor/mixins/archival_client.py
+++ pi_portal/modules/tasks/processor/mixins/archival_client.py
@@ -1,10 +1,18 @@
 """An archival client for archival task processors."""
 
-from pi_portal.modules.integrations.s3 import client as s3_client
+from typing import Type
+
+from pi_portal.modules.integrations.archival import (
+    ArchivalException,
+    TypeArchivalClient,
+)
+from pi_portal.modules.integrations.archival.service_client import (
+    ArchivalClient,
+)
 
 
 class ArchivalClientMixin:
   """An archival client for archival task processors."""
 
-  archival_client_class = s3_client.S3BucketClient
-  archival_client_exception_class = s3_client.S3BucketException
+  archival_client_class: Type[TypeArchivalClient] = ArchivalClient
+  archival_client_exception_class: Type[Exception] = ArchivalException
