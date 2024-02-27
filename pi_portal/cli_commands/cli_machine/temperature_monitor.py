1cf494d26dc63e812e56229f32c7add6db35cf9e
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-02-27 19:04:58 +0000

    feat(CLI): support requiring the task scheduler

--- pi_portal/cli_commands/cli_machine/temperature_monitor.py
+++ pi_portal/cli_commands/cli_machine/temperature_monitor.py
@@ -1,18 +1,21 @@
 """CLI command to start the Temperature Monitor."""
 
 from pi_portal.cli_commands.bases import command
-from pi_portal.cli_commands.mixins import state
+from pi_portal.cli_commands.mixins import require_task_scheduler, state
 from pi_portal.modules.integrations import gpio
 
 
 class TemperatureMonitorCommand(
-    command.CommandBase,
+    require_task_scheduler.CommandTaskSchedulerMixin,
     state.CommandManagedStateMixin,
+    command.CommandBase,
 ):
   """CLI command to start the Temperature Monitor."""
 
   def invoke(self) -> None:
     """Invoke the command."""
+
+    self.require_task_scheduler()
 
     factory = gpio.TemperatureSensorMonitorFactory()
     temperature_monitor = factory.create()
