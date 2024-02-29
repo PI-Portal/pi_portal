8fcc42d6278085d2b08a308f8e8bd87655d79ba1
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-02-29 17:47:30 +0000

    refactor(CAMERA): extract abc, motion client

---
+++ pi_portal/modules/integrations/camera/motion/client.py
@@ -0,0 +1,38 @@
+"""Camera client for the Motion application."""
+
+import logging
+
+from pi_portal.modules.integrations.camera.bases.client import (
+    CameraClientBase,
+    CameraException,
+)
+from pi_portal.modules.integrations.network import http
+
+
+class MotionClient(CameraClientBase):
+  """Camera client for the Motion application.
+
+  :param log: The logger instance to use.
+  """
+
+  snapshot_url = 'http://localhost:8080/{0}/action/snapshot'
+
+  def __init__(self, log: logging.Logger) -> None:
+    super().__init__(log)
+    self.http_client = http.HttpClient(self.log)
+    self.http_client.set_basic_auth(
+        self.current_state.user_config["MOTION"]["AUTHENTICATION"]["USERNAME"],
+        self.current_state.user_config["MOTION"]["AUTHENTICATION"]["PASSWORD"],
+    )
+
+  def take_snapshot(self, camera: int) -> None:
+    """Take a snapshot with Motion.
+
+    :param camera: The camera index to use.
+    :raises: :class:`CameraException`
+    """
+
+    try:
+      self.http_client.get(self.snapshot_url.format(camera))
+    except http.HttpClientError as exc:
+      raise CameraException("Unable to take snapshot.") from exc
