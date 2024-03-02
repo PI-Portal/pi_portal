f85cbb8f434769e9f52ca36dce95840e1f3686f4
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-03-02 18:46:20 +0000

    feat(TASKS): log task timing information

--- pi_portal/modules/tasks/processor/tests/test_queue_maintenance.py
+++ pi_portal/modules/tasks/processor/tests/test_queue_maintenance.py
@@ -12,12 +12,15 @@ from pi_portal.modules.tasks.processor.queue_maintenance import ProcessorClass
 class TestQueueMaintenanceProcessor:
   """Test the QueueMaintenanceProcessor class."""
 
-  log_message_prefix = "DEBUG - {task.id} - None - Processing '{task}' ...\n"
+  log_message_prefix = "DEBUG - {task.id} - None - Processing: '{task}' ...\n"
   log_message_maintenance = (
       "WARNING - {task.id} - {queue} - "
       "Performing maintenance on the '{queue}' task queue ...\n"
   )
-  log_message_suffix = "DEBUG - {task.id} - None - Completed '{task}'!\n"
+  log_message_suffix = (
+      "DEBUG - {task.id} - None - Completed: '{task}'!\n"
+      "DEBUG - {task.id} - None - Task Timing: '{task}'.\n"
+  )
 
   def test_initialize__attributes(
       self,
