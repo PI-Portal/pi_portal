17d12250ba40b928c7f3179695ad6add586fb352
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2024-03-16 19:12:56 +0000

    feat(INSTALLER): validate existing files and folders

--- pi_portal/installation/actions/tests/test_action_create_paths.py
+++ pi_portal/installation/actions/tests/test_action_create_paths.py
@@ -4,10 +4,14 @@ from io import StringIO
 from typing import List
 from unittest import mock
 
+import pytest
 from pi_portal.modules.python.mock import CallType
-from .. import action_create_paths
 from ..action_create_paths import CreatePathsAction, FileSystemPath
 from ..bases import base_action
+from .conftest import (
+    FilePathCreationScenario,
+    TypeFilePathCreationScenarioCreator,
+)
 
 
 class TestCreatePathsAction:
@@ -85,15 +89,18 @@ class TestCreatePathsAction:
         base_action.ActionBase,
     )
 
-  @mock.patch(
-      action_create_paths.__name__ + '.os.path.exists',
-      mock.Mock(return_value=True),
+  @pytest.mark.parametrize(
+      "scenario",
+      [FilePathCreationScenario(path_exists=True)],
   )
-  def test_invoke__existing__logging(
+  def test_invoke__existing__correct_type__logging(
       self,
       concrete_action_create_paths_instance: CreatePathsAction,
       mocked_stream: StringIO,
+      setup_file_path_creation_scenario: TypeFilePathCreationScenarioCreator,
+      scenario: FilePathCreationScenario,
   ) -> None:
+    setup_file_path_creation_scenario(scenario)
     expected_log_messages = ""
     for file_system_path in (
         concrete_action_create_paths_instance.file_system_paths
@@ -106,15 +113,40 @@ class TestCreatePathsAction:
 
     assert mocked_stream.getvalue() == expected_log_messages
 
-  @mock.patch(
-      action_create_paths.__name__ + '.os.path.exists',
-      mock.Mock(return_value=True),
+  @pytest.mark.parametrize(
+      "scenario",
+      [FilePathCreationScenario(path_exists=True)],
   )
-  def test_invoke__existing__file_system_calls(
+  def test_invoke__existing__correct_type__calls_os_path_isdir(
+      self,
+      concrete_action_create_paths_instance: CreatePathsAction,
+      mocked_os_path_isdir: mock.Mock,
+      setup_file_path_creation_scenario: TypeFilePathCreationScenarioCreator,
+      scenario: FilePathCreationScenario,
+  ) -> None:
+    setup_file_path_creation_scenario(scenario)
+    expected_isdir_calls: List[CallType] = []
+    for file_system_path in (
+        concrete_action_create_paths_instance.file_system_paths
+    ):
+      expected_isdir_calls += [mock.call(file_system_path.path)]
+
+    concrete_action_create_paths_instance.invoke()
+
+    assert mocked_os_path_isdir.mock_calls == expected_isdir_calls
+
+  @pytest.mark.parametrize(
+      "scenario",
+      [FilePathCreationScenario(path_exists=True)],
+  )
+  def test_invoke__existing__correct_type__file_system_calls(
       self,
       concrete_action_create_paths_instance: CreatePathsAction,
       mocked_file_system: mock.Mock,
+      setup_file_path_creation_scenario: TypeFilePathCreationScenarioCreator,
+      scenario: FilePathCreationScenario,
   ) -> None:
+    setup_file_path_creation_scenario(scenario)
     expected_fs_calls: List[CallType] = []
     for file_system_path in (
         concrete_action_create_paths_instance.file_system_paths
@@ -127,15 +159,41 @@ class TestCreatePathsAction:
 
     assert mocked_file_system.mock_calls == expected_fs_calls
 
-  @mock.patch(
-      action_create_paths.__name__ + '.os.path.exists',
-      mock.Mock(return_value=False),
+  @pytest.mark.parametrize(
+      "scenario",
+      [
+          FilePathCreationScenario(path_exists=True, wrong_path_index=0),
+          FilePathCreationScenario(path_exists=True, wrong_path_index=2),
+      ],
+  )
+  def test_invoke__existing__wrong_type_folder__raises_exception(
+      self,
+      concrete_action_create_paths_instance: CreatePathsAction,
+      setup_file_path_creation_scenario: TypeFilePathCreationScenarioCreator,
+      scenario: FilePathCreationScenario,
+  ) -> None:
+    scenario_mocks = setup_file_path_creation_scenario(scenario)
+
+    with pytest.raises(OSError) as exc:
+      concrete_action_create_paths_instance.invoke()
+
+    assert str(exc.value) == (
+        f"The path {scenario_mocks.wrong_path} exists, "
+        f"but it is not a {scenario_mocks.expected_type}."
+    )
+
+  @pytest.mark.parametrize(
+      "scenario",
+      [FilePathCreationScenario(path_exists=False)],
   )
   def test_invoke__non_existing__logging(
       self,
       concrete_action_create_paths_instance: CreatePathsAction,
       mocked_stream: StringIO,
+      setup_file_path_creation_scenario: TypeFilePathCreationScenarioCreator,
+      scenario: FilePathCreationScenario,
   ) -> None:
+    setup_file_path_creation_scenario(scenario)
     expected_log_messages = ""
     for file_system_path in (
         concrete_action_create_paths_instance.file_system_paths
@@ -148,15 +206,18 @@ class TestCreatePathsAction:
 
     assert mocked_stream.getvalue() == expected_log_messages
 
-  @mock.patch(
-      action_create_paths.__name__ + '.os.path.exists',
-      mock.Mock(return_value=False),
+  @pytest.mark.parametrize(
+      "scenario",
+      [FilePathCreationScenario(path_exists=False)],
   )
   def test_invoke__non_existing__file_system_calls(
       self,
       concrete_action_create_paths_instance: CreatePathsAction,
       mocked_file_system: mock.Mock,
+      setup_file_path_creation_scenario: TypeFilePathCreationScenarioCreator,
+      scenario: FilePathCreationScenario,
   ) -> None:
+    setup_file_path_creation_scenario(scenario)
     expected_fs_calls: List[CallType] = []
     for file_system_path in (
         concrete_action_create_paths_instance.file_system_paths
