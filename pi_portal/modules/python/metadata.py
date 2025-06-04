92f4c4bfae928f48aebaf1a948d7a8c1c8c7f89a
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-06-03 23:49:45 -0400

    feat(TYPES): squash me, this is weird error

--- pi_portal/modules/python/metadata.py
+++ pi_portal/modules/python/metadata.py
@@ -1,7 +1,8 @@
 """Compatibility shim for importlib across Python versions."""
 # pylint: disable=unused-import
+# mypy: allow-redefinition
 
 try:
   from importlib.metadata import version as metadata_version
 except ImportError:
-  from importlib_metadata import version as metadata_version
+  from importlib_metadata import version as metadata_versions
